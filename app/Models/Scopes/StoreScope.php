ecb2d9c2a7a14155fc03d6587b7a6491d9204d37
Marcelo Raineri <marcelo@gutlz.com>
2025-03-29 21:19:11 +0000

    feat(scope): apply multi tenant scope validation

---
+++ app/Models/Scopes/StoreScope.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Builder;
+
+class StoreScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            $builder->whereIn('store_id', $user->stores->pluck('id'));
+        }
+    }
+}
