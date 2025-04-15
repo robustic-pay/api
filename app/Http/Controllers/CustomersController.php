2f543f0accaa7448ecfd2090680d5dc0f4e0339a
Marcelo Raineri <marcelo@gutlz.com>
2025-04-15 04:27:12 +0100

    feat(controllers):  customer controller

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,49 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Store;
+use App\Models\Customers;
+use Illuminate\Support\Facades\Gate;
 use App\Http\Requests\StoreCustomersRequest;
 use App\Http\Requests\UpdateCustomersRequest;
-use App\Models\Customers;
 
 class CustomersController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Store $store)
     {
-        //
+        $customer = Customers::where('store_id', $store->id)->paginate();
+        return response()->json($customer, 200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCustomersRequest $request)
+    public function store(StoreCustomersRequest $request, Store $store) 
     {
-        //
+        $customer = Customers::createForStore($request->validated(), $store->id);
+        return response()->json($customer, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Customers $customers)
+    public function show(Store $store, Customers $customer)
     {
-        //
+        Gate::authorize('access', [$customer, $store]);
+        return response()->json($customer, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCustomersRequest $request, Customers $customers)
+    public function update(Store $store, UpdateCustomersRequest $request, Customers $customer)
     {
-        //
+        Gate::authorize('access', [$customer, $store]);
+        $customer->update($request->validated());
+        return response()->json($customer, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Customers $customers)
+    public function destroy(Store $store, Customers $customer)
     {
-        //
+        Gate::authorize('access', [$customer, $store]);
+        $customer->delete();
+        return response()->json(['message' => 'Customer successfully deleted.'], 200);
     }
 }
