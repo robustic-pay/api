b70551ea8caa2c624ac5f37f34039a872fda007d
Marcelo Raineri <marcelo@gutlz.com>
2025-04-15 02:22:00 +0100

    feat(migrations): removal of the unique field in the customer table

--- database/migrations/2025_04_10_150821_create_customers_table.php
+++ database/migrations/2025_04_10_150821_create_customers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
             $table->string('city')->nullable();
